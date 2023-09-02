017b83fbed06bdaf01dd1b7f10620b9c1950a6fd
rafael3007 <rafael.brito.1422@gmail.com>
2023-09-02 14:46:37 -0300

    operacoes

--- Aula 04/src/br/edu/ifba/subconjunto/App.java
+++ Aula 04/src/br/edu/ifba/subconjunto/App.java
@@ -1,7 +1,18 @@
 package br.edu.ifba.subconjunto;
 
+import br.edu.ifba.subconjunto.impl.OperacoesImpl;
+import br.edu.ifba.subconjunto.operacoes.Operacoes;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Operacoes<Integer> operacoes = new OperacoesImpl();
+
+        Integer[] numeros = operacoes.gerarNumeros(1000);
+
+        if (operacoes.encontrar(numeros, 9999999)) {
+            System.out.println("Soma de subconjunto encontrada!");
+        } else {
+            System.out.println("soma de subconjunto não encontrada!");
+        }
     }
 }
